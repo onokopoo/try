b2c69eca5066e7980a63cd6dfd005cca6ee26f93
onokopoo <onokopoo@gmail.com>
2015-11-16 16:48:48 +0700

    vn,

--- app/src/main/java/onokopoo/kanom/MainActivity.java
+++ app/src/main/java/onokopoo/kanom/MainActivity.java
@@ -6,7 +6,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
@@ -15,15 +19,54 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ArrayList<HashMap<String, String>> MyArrList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,12 +123,19 @@ public class MainActivity extends AppCompatActivity
         navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        //
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        ShowData();
+
+        final Button btnSearch = (android.widget.Button) findViewById(R.id.btnSearch);
+        btnSearch.setBackgroundColor(Color.TRANSPARENT);
+        // Perform action on click
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
-                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
-                startActivity(i);
+                ShowData();
             }
         });
     }
@@ -156,4 +206,217 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void ShowData()
+    {
+        // listView1
+        final ListView lisView1 = (ListView)findViewById(R.id.listView1);
+
+        // keySearch
+        EditText strKeySearch = (EditText)findViewById(R.id.txtKeySearch);
+        // Disbled Keyboard auto focus
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(strKeySearch.getWindowToken(), 0);
+
+        //String url = "http://10.35.23.50/pop.php";
+        String url = getString(R.string.url)+"/pop.php";
+
+        // Paste Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("txtKeyword", strKeySearch.getText().toString()));
+
+        try {
+            JSONArray data = new JSONArray(getJSONUrl(url,params));
+
+            MyArrList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> map;
+
+            for(int i = 0; i < data.length(); i++){
+                JSONObject c = data.getJSONObject(i);
+
+                map = new HashMap<String, String>();
+                map.put("image", c.getString("image"));
+                map.put("kanom_id", c.getString("kanom_id"));
+                map.put("name_th", c.getString("name_th"));
+                map.put("name_en", c.getString("name_en"));
+                map.put("type", c.getString("type"));
+                map.put("voice", c.getString("voice"));
+                MyArrList.add(map);
+            }
+
+            lisView1.setAdapter(new ImageAdapter(this,MyArrList));
+
+            // OnClick Item
+            lisView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> myAdapter, View myView,
+                                        int position, long mylng) {
+
+                    String sImage = MyArrList.get(position).get("image").toString();
+                    String sKanom_id = MyArrList.get(position).get("kanom_id").toString();
+                    String sNameTH = MyArrList.get(position).get("name_th").toString();
+                    String sNameEN = MyArrList.get(position).get("name_en").toString();
+                    String sType = MyArrList.get(position).get("type").toString();
+                    String sVoice = MyArrList.get(position).get("name_en").toString();
+
+                    Intent newActivity = new Intent(MainActivity.this,DetailActivity.class);
+                    newActivity.putExtra("kanom_id", sKanom_id);
+                    startActivity(newActivity);
+
+                }
+            });
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public class ImageAdapter extends BaseAdapter
+    {
+        private Context context;
+        private ArrayList<HashMap<String, String>> MyArr = new ArrayList<HashMap<String, String>>();
+
+        public ImageAdapter(Context c, ArrayList<HashMap<String, String>> list)
+        {
+            // TODO Auto-generated method stub
+            context = c;
+            MyArr = list;
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return MyArr.size();
+        }
+
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.activity_column, null);
+            }
+
+            // image
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.image);
+            imageView.getLayoutParams().height = 250;
+            imageView.getLayoutParams().width = 800;
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            try
+            {
+                imageView.setImageBitmap(loadBitmap(getString(R.string.url)+MyArr.get(position).get("image")));
+            } catch (Exception e) {
+                // When Error
+                imageView.setImageResource(android.R.drawable.ic_menu_report_image);
+
+            }
+
+            // name th
+            TextView txtNameTh = (TextView) convertView.findViewById(R.id.name_th);
+            txtNameTh.setPadding(10, 0, 0, 0);
+            txtNameTh.setText(MyArrList.get(position).get("name_th"));
+
+            // name en
+            TextView txtNameEn = (TextView) convertView.findViewById(R.id.name_en);
+            txtNameEn.setPadding(5, 0, 0, 0);
+            txtNameEn.setText(MyArrList.get(position).get("name_en"));
+
+            // type
+            TextView txtType = (TextView) convertView.findViewById(R.id.type);
+            txtType.setPadding(5, 0, 0, 0);
+            txtType.setText(MyArrList.get(position).get("type"));
+
+            return convertView;
+
+        }
+    }
+
+    public String getJSONUrl(String url,List<NameValuePair> params) {
+        StringBuilder str = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) { // Download OK
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                }
+            } else {
+                Log.e("Log", "Failed to download result.............");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+    private static final String TAG = "ERROR";
+    private static final int IO_BUFFER_SIZE = 4 * 1024;
+    public static Bitmap loadBitmap(String url) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        BufferedOutputStream out = null;
+
+        try {
+            in = new BufferedInputStream(new URL(url).openStream(), IO_BUFFER_SIZE);
+
+            final ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+            out = new BufferedOutputStream(dataStream, IO_BUFFER_SIZE);
+            copy(in, out);
+            out.flush();
+
+            final byte[] data = dataStream.toByteArray();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            //options.inSampleSize = 1;
+
+            bitmap = BitmapFactory.decodeByteArray(data, 0, data.length,options);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not load Bitmap from: " + url);
+        } finally {
+            closeStream(in);
+            closeStream(out);
+        }
+
+        return bitmap;
+    }
+
+    private static void closeStream(Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                android.util.Log.e(TAG, "Could not close stream", e);
+            }
+        }
+    }
+
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] b = new byte[IO_BUFFER_SIZE];
+        int read;
+        while ((read = in.read(b)) != -1) {
+            out.write(b, 0, read);
+        }
+    }
+
 }
