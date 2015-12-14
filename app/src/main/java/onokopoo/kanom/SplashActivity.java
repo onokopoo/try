63c869ac1f7a373a6dfb7dab52155763053b8c5b
onokopoo <onokopoo@gmail.com>
2015-12-14 13:50:20 +0700

    update

--- app/src/main/java/onokopoo/kanom/SplashActivity.java
+++ app/src/main/java/onokopoo/kanom/SplashActivity.java
@@ -5,21 +5,20 @@ package onokopoo.kanom;
  */
 
 import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.view.Window;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class SplashActivity extends Activity {
     Handler handler;
     Runnable runnable;
-    long delay_time;
-    long time = 1000L;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splashscreen);
+
         handler = new Handler();
 
         runnable = new Runnable() {
@@ -33,14 +32,11 @@ public class SplashActivity extends Activity {
 
     public void onResume() {
         super.onResume();
-        delay_time = time;
-        handler.postDelayed(runnable, delay_time);
-        time = System.currentTimeMillis();
+        handler.postDelayed(runnable, 3000);
     }
 
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
         handler.removeCallbacks(runnable);
-        time = delay_time - (System.currentTimeMillis() - time);
     }
 }
